9b68000d758994ddf26666bed53e52d8aef4d8d8
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-29 14:45:50 -0400

    added Maps view with correct showing of map, deleted some css lines that were messing up map rendering and passed map front end JS to yes.js

--- public/js/yes.js
+++ public/js/yes.js
@@ -4,16 +4,16 @@
 document.addEventListener('DOMContentLoaded', (event) =>  {
   console.log('DOM fully loaded');
 
-  function initMap() {
-    let uluru = {lat: -25.363, lng: 131.044};
-    let map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: uluru
-    });
-    let marker = new google.maps.Marker({
-      position: uluru,
-      map: map
-    });
-  }
-
 });
+function initMap() {
+  let uluru = {lat: 40.730610, lng: -73.935242};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 7,
+    center: uluru
+  });
+  let marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
