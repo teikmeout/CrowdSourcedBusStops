a969891ba69e1c83ea63521790dff53d76a6e383
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-29 11:14:21 -0400

    added map specifics to yes.js

--- public/js/yes.js
+++ public/js/yes.js
@@ -2,7 +2,18 @@
 
 // DOM load listener
 document.addEventListener('DOMContentLoaded', (event) =>  {
-    console.log('DOM fully loaded');
+  console.log('DOM fully loaded');
 
+  function initMap() {
+    let uluru = {lat: -25.363, lng: 131.044};
+    let map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 4,
+      center: uluru
+    });
+    let marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    });
+  }
 
 });
