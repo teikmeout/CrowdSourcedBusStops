36133050db22dd5c62f10cb4301484f86f2e21fe
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-30 13:26:56 -0400

    modified routes for auth

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,21 @@
 // using express routes in this file
-const router = require('express').Router();
+const indexRouter = require('express').Router();
 
 // HOME route
-router.get('/', (req, res) => {
+indexRouter.get('/', (req, res) => {
+  // rafa has a choosing page for login and signup, I don't
   res.render('index');
 }); // end of router
 
-module.exports = router;
+// This route serves your `/login` form
+indexRouter.get('/login', (req, res) => {
+  // rafa has a landing load page, I only have sign in
+  res.render('index');
+});
+
+// This route serves your `/signup` form
+indexRouter.get('/signup', (req, res) => {
+  res.render('signup');
+});
+
+module.exports = indexRouter;
--- server.js
+++ server.js
@@ -3,35 +3,55 @@
 // to allow console log
 /* eslint no-console: ["error", { allow: ["warn", "error"] }] */
 
-/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
-
-const express     = require('express');
-const logger      = require('morgan');
-const dotEnv      = require('dotenv').config({silent: true});
-
-// calling in a new instance of express
-const app         = express();
-// "importing" path package
-const path        = require('path');
-// setting up ports for server
-const port        = process.env.PORT || 3000;
+const dotEnv           = require('dotenv').config({silent: true});
+const express          = require('express');
+const logger           = require('morgan');
+const path             = require('path');
+const bodyParser       = require('body-parser');
+const session          = require('express-session');
+const cookieParser     = require('cookie-parser');
+const methodOverride   = require('method-override');
 
 // setting up routes
-const homeRoute   = require('./routes/home');
-const mapRoute    = require('./routes/map');
-const geoRoute    = require('./routes/geo');
+const homeRoute        = require('./routes/home');
+const mapRoute         = require('./routes/map');
+const geoRoute         = require('./routes/geo');
+const authRouter       = require('./routes/auth');
+
+// calling in a new instance of express
+const app              = express();
+// setting up ports for server
+const port             = process.env.PORT || 3000;
+const SECRET           = 'taka3000';
 
 
-// setting logger flavor to DEV
-app.use(logger('dev'));
 // setting views to use ejs
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
+// setting morgan flavor to DEV
+app.use(logger('dev'));
+// middleware for method override
+app.use(methodOverride('_method'));
+// This is how we read the cookies sent over from the browser
+app.use(cookieParser());
+// session creating
+app.use(session({
+  resave: false,
+  saveUninitialized: false,
+  secret: SECRET
+}));
+
 // telling express that all static files come from this folder
 app.use(express.static(path.join(__dirname, 'public')));
 
 // using correct routes
 app.use('/', homeRoute);
+app.use('/auth', authRouter);
 app.use('/map', mapRoute);
 app.use('/geo', geoRoute);
 
