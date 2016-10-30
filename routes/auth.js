568404a3b5f4e154679f002ae5e31b8ea80e03e7
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-30 13:26:06 -0400

    working on login process

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+/* eslint no-multi-spaces: ["error", { exceptions: { "VariableDeclarator": true } }] */
+/* eslint no-param-reassign: ["error", { "props": false }] */
+
+const authRouter    = require('express').Router();
+const { logIn }     = require('../lib/auth');
+
+/**
+ * Log In and if successful assign res.user._id to the session
+ * It uses the logIn middleware from the auth library to parse the form inputs
+ * and save the user to the database
+ */
+authRouter.post('/', logIn, (req, res) => {
+  res.redirect('/users/profile');
+});
+
+// Logout by assigning null to the userId in the session
+authRouter.delete('/', (req, res) => {
+  req.session.userId = null;
+  res.redirect('/');
+});
+
+module.exports = authRouter;
+
+// NOTE: this is code provided by Rafa Pacas, I have no dominated login process at all.
