0ade77b726b067f66647418043cca5dbebc2045c
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-10-31 13:55:09 -0400

    log in and log out working

--- routes/map.js
+++ routes/map.js
@@ -2,17 +2,19 @@
 const router = require('express').Router();
 
 // this code is commented because it's the iTunes implementation
-// const { authenticate }    = require('../lib/auth');
+const { authenticate }    = require('../lib/auth');
 // const { searchMusic }     = require('../services/itunes');
-// const { getFavorites,
-//         saveFavorite,
-//         deleteFavorites } = require('../models/favorites');
+const { getFavorites,
+        saveFavorite,
+        deleteFavorites } = require('../models/favorites');
 
 // HOME route
-router.get('/', (req, res) => {
+// I NEED TO ADD authenticate to this process
+router.get('/', authenticate, (req, res) => {
   res.render('map', {
     // this part was taken from @smna15 since I didn't figure out how to hide the map key on my own
     API_KEY: process.env.MAPS_KEY,
+    user: res.user,
   });
 }); // end of router
 
@@ -20,13 +22,13 @@ module.exports = router;
 
 // CODE commented off for reference and creating of routes to work through
 // pret a manger bathroom code 8701
-// router.get('/', authenticate, getFavorites, (req, res) => {
-//   res.render('music/index', {
-//     user: res.user,
-//     results: res.results || [],
-//     favorites: res.favorites || []
-//   });
-// });
+router.get('/', authenticate, getFavorites, (req, res) => {
+  res.render('/users', {
+    user: res.user,
+    results: res.results || [],
+    favorites: res.favorites || []
+  });
+});
 
 // router.post('/search', authenticate, searchMusic, getFavorites, (req,res) => {
 //   res.render('music/index', {
