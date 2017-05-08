dc963131211a940b39c7372994c524abf00da647
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-05-08 13:27:16 -0400

    separated routes into their own file

---
+++ router.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+
+// setting correct routes
+router.use('/', require('./routes/home'));
+router.use('/auth', require('./routes/auth'));
+router.use('/map', require('./routes/map'));
+router.use('/geo', require('./routes/geo'));
+router.use('/users', require('./routes/users')); // takes me to create user or show profile
+router.use('*', (req, res) => {
+  res.send('fucked up dude')
+});
+
+module.exports = router;
