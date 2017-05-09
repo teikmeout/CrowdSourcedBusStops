d44affa256fe1c00ef7283079e8004f951efb290
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2017-05-09 07:45:37 -0400

    added default route

--- router.js
+++ router.js
@@ -6,9 +6,10 @@ router.use('/', require('./routes/home'));
 router.use('/auth', require('./routes/auth'));
 router.use('/map', require('./routes/map'));
 router.use('/geo', require('./routes/geo'));
-router.use('/users', require('./routes/users')); // takes me to create user or show profile
+router.use('/users', require('./routes/users'));
+// takes me to create user or show profile
 router.use('*', (req, res) => {
-  res.send('fucked up dude')
+  res.send('fucked up dude');
 });
 
 module.exports = router;
