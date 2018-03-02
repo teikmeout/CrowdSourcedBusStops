60daeb2c33fdc9ef187f52ebe12237778d804ded
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2018-03-02 00:06:43 -0500

    changed favorites middleware to psql interaction

--- models/favorites.js
+++ models/favorites.js
@@ -1,84 +1,109 @@
-// this model interacts with the DB.
-// please TURN ON MONGOD
+const db = require('../lib/dbConnect.js');
 
-// const { ObjectID } = require('mongodb');
-// const { getDB }    = require('../lib/dbConnect.js');
-
-// const DB_CONNECTION = 'mongodb://localhost:27017/itunescrud';
-
-const db = require('./db.js');
-
-// function getFavorites(req, res, next) {
-//   console.log(req.session.userId);
-//   // find all favorites for your userId
-//   getDB().then((db) => {
-//     db.collection('favorites')
-//       .find({ userId: { $eq: req.session.userId } })
-//       .toArray((toArrErr, data) => {
-//         if(toArrErr) return next(toArrErr);
-//         res.favorites = data;
-//         db.close();
-//         next();
-//       });
-//       return false;
-//   });
-//   return false;
+// function getAllLocations(req, res, next) {
+//   console.log('getAllLocations');
+//   db.any(`SELECT * FROM locations;`)
+//   .then((allLocations) => {
+//     res.allLocations = allLocations;
+//     next();
+//   })
+//   .catch((err) => {
+//     next(err);
+//   })
 // }
 
-function getAllLocations(req, res, next) {
-  console.log('getAllLocations');
-  db.any(`SELECT * FROM locations;`)
-  .then((allLocations) => {
-    res.allLocations = allLocations;
-    next();
-  })
-  .catch((err) => {
-    next(err);
-  })
+// READ
+// vv replaces getFavorites
+function getLocationsByUser(req, res, next) {
+  console.log('getLocationsByUser');
+  // we're using session to contain the logged in user
+  // checking value of session to see what the userId is
+  console.log('session ->', req.session);
+  db.any(`SELECT * FROM locations WHERE user_id = $1;`, [req.session.userId])
+    .then((locations) => {
+      console.log('locations by user ->', locations);
+      res.favorites = locations;
+      next();
+    })
+    .catch((err) => {
+      next(err);
+    });
 }
 
+// CREATE
 function saveFavorite(req, res, next) {
   // creating an empty object for the insertObj
   const insertObj = {};
 
   // copying all of req.body into insertObj
-  for(key in req.body) {
+  for(let key in req.body) {
     insertObj[key] = req.body[key];
   }
 
   // Adding userId to insertObj
-  insertObj.favorite.userId = req.session.userId;
+  insertObj.user_id = req.session.userId;
 
-  getDB().then((db) => {
-    db.collection('favorites')
-      .insert(insertObj.favorite, (insertErr, result) => {
-        if (insertErr) return next(insertErr);
-        res.saved = result;
-        db.close();
-        next();
-      });
-      return false;
-  });
-  return false;
+  // getDB().then((db) => {
+  //   db.collection('favorites')
+  //     .insert(insertObj.favorite, (insertErr, result) => {
+  //       if (insertErr) return next(insertErr);
+  //       res.saved = result;
+  //       db.close();
+  //       next();
+  //     });
+  //     return false;
+  // });
+  // return false;
+
+  db.one(`INSERT INTO locations (lat, long, user_id)
+    VALUES ($[lat], $[long], $[user_id])
+    RETURNING *;`, insertObj)
+    .then((savedLocation) => {
+      console.log('location inserted correctly', savedLocation);
+      res.locals.savedLocation = savedLocation;
+      next();
+    })
+    .catch((err) => {
+      console.log('location insertion failed', err);
+      // res.status(500).json({
+      //   message: 'failed inserting at saveFavorite',
+      //   err,
+      // });
+      next(err);
+    });
 }
 
 // Delete method doesn't change because we are deleting objects from the database
 // based on that object's unique _id - you do not need to specify which user as
 // the _id is sufficient enough
 function deleteFavorites(req, res, next) {
-  getDB().then((db) => {
-    db.collection('favorites')
-      .findAndRemove({ _id: ObjectID(req.params.id) }, (removeErr, result) => {
-        if (removeErr) return next(removeErr);
-        res.removed = result;
-        db.close();
-        next();
-      });
-      return false;
-  });
-  return false;
+  db.one(`DELETE FROM locations WHERE id = $1;`, req.params.id)
+    .then((deletedLocation) => {
+      console.log('location delted correctly', deletedLocation);
+      res.locals.deletedLocation = deletedLocation;
+      next();
+    })
+    .catch((err) => {
+      console.log('location deletion failed');
+      next(err);
+    });
+  // getDB().then((db) => {
+  //   db.collection('favorites')
+  //     .findAndRemove({ _id: ObjectID(req.params.id) }, (removeErr, result) => {
+  //       if (removeErr) return next(removeErr);
+  //       res.removed = result;
+  //       db.close();
+  //       next();
+  //     });
+  //     return false;
+  // });
+  // return false;
 }
 
-module.exports = { getFavorites, saveFavorite, deleteFavorites };
+module.exports = {
+  getLocationsByUser,
+  saveFavorite,
+  deleteFavorites,
+};
 
 // code taken from @rapala61 template
